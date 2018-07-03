249b21140f572fc3bf5a7c4881278a8d89fbf884
Irec <ceribit@gmail.com>
2018-07-02 23:11:15 -0500

    Added a notes model and slightly improved the html

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Notes(models.Model):
+    body = models.TextField()
+    due_date = models.DateField()
+    created_at = models.DateTimeField(default = datetime.now, blank = True)
+
+    def days_left(self):
+        numdays = self.due_date.day - datetime.today().day
+        if numdays == 1:
+            return "1 day"
+        else:
+            return str(numdays) + " days"
+
+    def __str__(self):
+        return self.body
+    class Meta:
+        verbose_name_plural = "Note Data"
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Notes
+from datetime import date
 # Create your views here.
 def home(request):
-    return render(request, 'dashboard/home.html')
+    note_list = Notes.objects.all()[:10]
+    context = {
+        'notes' : note_list
+    }
+    return render(request, 'dashboard/home.html', context)
