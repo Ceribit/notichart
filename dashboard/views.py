8a8828aee6738c9d89d8653f76e3de305ff1ca59
Irec <ceribit@gmail.com>
2018-07-03 03:15:11 -0500

    Added RSS feed onto the main page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import feedparser
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,7 +9,9 @@ from datetime import date
 # Create your views here.
 def home(request):
     note_list = Notes.objects.all()[:10]
+    feeds = feedparser.parse('https://www.novelupdates.com/rss.php?uid=101502&unq=59e2dfbf49d04&type=0&lid=local')
     context = {
-        'notes' : note_list
+        'notes' : note_list,
+        'feeds' : feeds
     }
     return render(request, 'dashboard/home.html', context)
