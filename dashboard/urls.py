2f1429cc4f413201b707080387cbdb1288f22e8b
Irec <ceribit@gmail.com>
2018-07-05 00:31:58 -0500

    Added option to delete notes from home page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^', views.home, name = 'home')
+    url(r'^$', views.home, name = 'home'),
+    url(r'^remove_note/(?P<pk>\d+)/', views.remove_note, name = 'remove_note')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 import feedparser
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Notes
 from .forms import NoteForm
 from datetime import date
-# Create your views here.
+
+# HOMEVIEW: Takes care of adding notes and viewing the home page
 def home(request):
     if request.method == "POST":
         # Add form data to database
@@ -22,6 +23,7 @@ def home(request):
             ).save()
         return HttpResponseRedirect('/')
     else:
+        # Create context and render the screen
         form = NoteForm()
         note_list = Notes.objects.all()[:10]
         entries = feedparser.parse('https://www.novelupdates.com/rss.php?uid=101502&unq=59e2dfbf49d04&type=0&lid=local').entries[:10]
@@ -31,3 +33,22 @@ def home(request):
             'form' : form
         }
         return render(request, 'dashboard/home.html', context)
+
+# REMOVE_NOTE: Takes care of removing note then redirects back to the home page
+def remove_note(request, pk):
+    print('THIS DID RUN')
+    if request.method == 'POST':
+        note = get_object_or_404(Notes, pk=pk)
+        if request.method == 'POST':
+            note.delete()
+            return HttpResponseRedirect('/')
+
+    form = NoteForm()
+    note_list = Notes.objects.all()[:10]
+    entries = feedparser.parse('https://www.novelupdates.com/rss.php?uid=101502&unq=59e2dfbf49d04&type=0&lid=local').entries[:10]
+    context = {
+        'notes' : note_list,
+        'entries' : entries,
+        'form' : form
+    }
+    return HttpResponseRedirect('/')
